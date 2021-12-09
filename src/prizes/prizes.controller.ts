46bfb08ba2273f83e33b3ddf203f73705fa8a1c4
Valek2412 <valoleg99@gmail.com>
2021-12-09 17:15:15 +0200

    hotfix

--- src/prizes/prizes.controller.ts
+++ src/prizes/prizes.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { PrizesService } from './prizes.service';
 import { CreatePrizeDto } from './dto/createPrize.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('prizes')
 export class PrizesController {
   constructor(private prizesService: PrizesService) {}
 
   @Post()
-  createPrize(@Body() dto: CreatePrizeDto) {
-    const prize = this.prizesService.createPrize(dto);
+  @UseInterceptors(FileInterceptor('image'))
+  createPrize(@Body() dto: CreatePrizeDto, @UploadedFile() image) {
+    const prize = this.prizesService.createPrize(dto, image);
     return prize;
   }
 
